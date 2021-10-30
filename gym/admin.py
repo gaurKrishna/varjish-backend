69113d3a787a46806e9d2106d20764a66e786493
Krishna Gaur <gaurkrishna498@gmail.com>
2021-10-30 17:13:54 +0530

    Added models to admin panel

--- gym/admin.py
+++ gym/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (
+    Gym,
+    Trainer,
+    Trainee,
+    DietAndWorkout
+)
 
+admin.site.register(Gym)
+admin.site.register(Trainer)
+admin.site.register(Trainee)
+admin.site.register(DietAndWorkout)
 # Register your models here.
