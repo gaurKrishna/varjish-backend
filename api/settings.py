71bf59272aedb9052532642abddfbc0b9f412f4b
Krishna Gaur <gaurkrishna498@gmail.com>
2021-10-30 17:30:43 +0530

    Added python decouple

--- api/settings.py
+++ api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import django_heroku
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ky7yf0s*6r)bk1lu&y17**t0bc_q!7e!q0kr7e&!9p=l@dlfq('
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = ['varjish-backend.herokuapp.com']
 
